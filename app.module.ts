947b8df2e2c93d9ed8d6b6a2cce3edd2eeb39762
jnjijij <136825012+jnjijij@users.noreply.github.com>
2023-12-07 17:48:10 +0200

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,28 @@
+// app.module.ts
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        UserListComponent,
+        UserDetailsComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
